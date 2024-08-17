6b442af857ecdb58699be080bb95771dd095bfe4
William Hart <goodwillhart@googlemail.com>
2024-08-17 01:29:19 +0100

    Lift formulas to a line structure within the context object.

--- context.js
+++ context.js
@@ -1,12 +1,12 @@
 // context.js
 
 let context = {
-    formulas: []
+    lines: []
 };
 
 export function initializeContext() {
     context = {
-        formulas: []
+        lines: []
     };
     return context;
 }
@@ -17,5 +17,24 @@ export function getContext() {
 
 export function updateContext(newContext) {
     context = newContext;
+}
+
+export function addLine(formula, target = false, assumptions = [], proved = true, justification = ['Hyp', []]) {
+    const newLine = {
+        formula: formula,
+        target: target,
+        assumptions: assumptions,
+        proved: proved,
+        justification: justification
+    };
+    context.lines.push(newLine);
+}
+
+export function getLine(index) {
+    return context.lines[index];
+}
+
+export function updateLine(index, updatedLine) {
+    context.lines[index] = updatedLine;
 }
 
